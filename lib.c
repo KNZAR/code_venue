8226feec23ddcd961523f42f8f2a779a16dcdd16
Malek Zayed <malekamrzayed311@gmail.com>
2024-06-09 17:04:15 +0100

    updated input function

--- lib.c
+++ lib.c
@@ -1,11 +1,20 @@
+// a "safer" input function
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-char* input(const char* prompt)
+#define FAILED_ALLOCATING NULL
+#define REALLOC_FAILURE -1
+#define FAILED_TRIMMING -2
+
+/* returns null if allocation for string fails, returns uncompleted string if array resizing fails
+   returns string with extra memory blocks if trimming fails, signals user with error pointer
+*/
+char* input(const char* prompt, int *error)
 {
+    *error = 0;
     // checks if prompt is empty string or null
-    if(strcmp(prompt, "") != 0 && prompt != NULL)
+    if((prompt != NULL && strcmp(prompt, "") != 0))
         printf("%s", prompt);
 
 
@@ -13,6 +22,11 @@ char* input(const char* prompt)
     int char_count = 0;
     char c;
     char *str = malloc(sizeof(char) * mut_buffer);
+    {
+        free(str);
+        return FAILED_ALLOCATING;
+    }
+
     if(str == NULL)
         return NULL;
 
@@ -22,18 +36,20 @@ char* input(const char* prompt)
         // checks if the string will be full next iteration
         if(char_count == mut_buffer - 1)
         {
-            mut_buffer += 10;
+            mut_buffer *= 2;
 
             // copy pointer if original pointer is lost if realloc fails
             char *cp = str;
-            // increase size of string by 10
+            
+            // resize array
             str = realloc(str, sizeof(char) * mut_buffer);
 
-            // realloc failure
+            // returns string midway typing if failed to resize
             if(str == NULL)
             {
-                free(cp);
-                return NULL;
+                *error = REALLOC_FAILURE;
+                return cp;
+
             }
         }
         // adds most recently read character
@@ -43,14 +59,15 @@ char* input(const char* prompt)
 
     // terminates our string
     str[char_count] = '\0';
-    int str_length = strlen(str);
     
     // string with exact length without extra memory from realloc calls
-    char* trimmed_str = malloc(sizeof(char) * (str_length + 1));
+    char* trimmed_str = malloc(sizeof(char) * (char_count + 1));
+
+    // returns string with extra memory if malloc fails
     if(trimmed_str == NULL)
     {
-        free(str);
-        return NULL;
+        *error = FAILED_TRIMMING;
+        return str;
     }
     strcpy(trimmed_str, str);
     free(str);
